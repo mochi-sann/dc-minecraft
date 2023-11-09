fb2f0eb9d58af140e8f3fadb5ed909f81f5c9575
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-11-09 22:51:13 +0900

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { createBot, Intents, startBot } from "discorddeno";
+import "https://deno.land/std@0.205.0/dotenv/load.ts";
+import {
+  enableCachePlugin,
+  enableCacheSweepers,
+} from "https://deno.land/x/discordeno_cache_plugin@0.0.21/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const baseBot = createBot({
+  token: Deno.env.get("DISCORD_TOKEN")!,
+  intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent,
+
+  events: {
+    ready() {
+      console.log("Successfully connected to gateway");
+    },
+    messageCreate(bot, message) {
+      if (message.content.startsWith("!ping")) {
+        bot.helpers.sendMessage(message.channelId, { content: "pong!" });
+      }
+    },
+  },
+});
+
+await startBot(baseBot);
