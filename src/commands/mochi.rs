b8c89ce3c4df620851bac2f0f41e74a3b921bf35
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-11-12 21:48:23 +0900

    色々した

--- src/commands/mochi.rs
+++ src/commands/mochi.rs
@@ -1,4 +1,5 @@
-use poise::serenity_prelude::Embed;
+use poise::serenity_prelude::{ButtonStyle, Embed};
+use serenity::{builder::CreateButton, model::Timestamp};
 
 use crate::{Context, Error};
 
@@ -12,15 +13,27 @@ pub async fn mochi(ctx: Context<'_>) -> Result<(), Error> {
             .description("Making a basic embed")
             .field("A field", "Has some content.", false)
     });
+    let mut button = CreateButton::default();
+    button
+        .custom_id("hello world")
+        .label("hello world")
+        .emoji('\u{2b55}')
+        .style(ButtonStyle::Primary);
 
+    // ctx.channel_id()
+    //     .send_message(&ctx.http(), |m| {
+    //         m.content("モチモチﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)")
+    //             .embed(|e| e.title("モチモチﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)"))
+    //     })
+    //     .await?;
     ctx.channel_id()
-        .send_message(&ctx.http(), |m| {
-            m.embed(|e| {
-                e.colour(0x00ff00)
-                    .title(format!("Stats for ", ))
-            })
+        .send_message(&ctx, |m| {
+            m.content("Please select your favorite animal")
+                .components(|c| c.create_action_row(|row| row.add_button(button)))
         })
-        .await?;
+        .await
+        .unwrap();
+    ctx.say("").await?;
 
     return Ok(());
 }
